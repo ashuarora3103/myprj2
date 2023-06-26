b756605b5a0424956c489eec6d1cc9a5e2addbef
ashuarora3103 <ashuarora3103@gmail.com>
2023-06-26 22:56:00 +0530

    first commit

---
+++ Consecutivesum.py
@@ -0,0 +1,11 @@
+limit=int(input("Limit: "))
+i=1
+my_sum  =0
+sum_string = ""
+while my_sum <= limit:
+      sum_string += f"{i}"
+      my_sum += i
+      i +=1
+if my_sum <= limit:
+    sum_string +=" + "
+print(f"The consecutive sum: {sum_string}= {my_sum}")
